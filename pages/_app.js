faeba7d21711f66ee9ce259e156581e2dfa55cd6
haohao1029 <ganjinghao2000@gmail.com>
2023-01-14 14:56:48 +0800

    feat: add color theme in _app.js and update card_expeses.main

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    darkblue: "#6001FF",
+    purple: "#6001FF",
+    pink: "#F890E5",
+    darkpink: "#E044C4",
+  },
+  style: {
+    global: {
+      'html, body': {
+        backgroundColor: '#6BCDE7',
+      }
+    }
+  }
+};
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
