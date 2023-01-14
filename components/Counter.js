b91bcb7ed1d0c35368666bec62953dc2aa8c655d
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-01-14 14:40:28 +0800

    setup ui components

---
+++ components/Counter.js
@@ -0,0 +1,14 @@
+const Counter = ({ count, label, testid }) => {
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <div
+        className="w-14 h-14 text-xl font-medium rounded-full bg-white inline-flex justify-center items-center"
+        data-testid={testid}
+      >
+        {count}
+      </div>
+      <span className="text-xs text-white">{label}</span>
+    </div>
+  );
+};
+export default Counter;
