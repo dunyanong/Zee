1d472ad7a89ad2710fc796e2f6487a6a7cb3d7b1
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-01-14 17:22:11 +0800

    add expense card

---
+++ components/TotalSpend.js
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+
+function TotalSpend() {
+  return (
+    <>
+      <Text className="text-[#5452C6] bg-[#F890E5]  focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-[20px] text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600  dark:focus:ring-blue-800">
+        Total Spend
+      </Text>
+    </>
+  );
+}
+
+export default TotalSpend;
