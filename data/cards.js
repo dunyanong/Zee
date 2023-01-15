f9f0207bac2b48645e0ad2ab64ed5d9c81e0cfea
Knazran <khairulnazran94@gmail.com>
2023-01-15 10:41:43 +0800

    new data

--- data/cards.js
+++ data/cards.js
@@ -1,12 +1,12 @@
 const CARDS = [
-  { id: 0, emoji: "🍅", price: "RM50", name: "Petrol", color: "#E42100" },
-  { id: 1, emoji: "🍊", price: "RM130", name: "Sephora", color: "#F36000" },
-  { id: 2, emoji: "🍋", price: "RM100", name: "Wifi", color: "#F3BC00" },
-  { id: 3, emoji: "🍐", price: "RM150", name: "Shopee", color: "#A0A226" },
-  { id: 4, emoji: "🥬", price: "RM100", name: "Electrical Bill", color: "#349B19" },
-  { id: 5, emoji: "🫐", price: "RM200", name: "Lobster", color: "#70BBFF" },
-  { id: 6, emoji: "🍆", price: "RM50", name: "Water Bill", color: "#7F4877" },
-  { id: 7, emoji: "🍇", price: "RM200", name: "Saving Plan", color: "#BC2A6E" }
+  { id: 0, emoji: "", price: "RM50", name: "Water Bill", color: "#7F4877" },
+  { id: 1, emoji: "", price: "RM130", name: "Sephora", color: "#F36000" },
+  { id: 2, emoji: "", price: "RM55.50", name: "Netflix", color: "#F3BC00" },
+  { id: 3, emoji: "", price: "RM150", name: "Shopee", color: "#A0A226" },
+  { id: 4, emoji: "", price: "RM100", name: "Village Grocer", color: "#349B19" },
+  { id: 5, emoji: "", price: "RM200", name: "Burger & Lobster", color: "#70BBFF" },
+  { id: 6, emoji: "", price: "RM50", name: "Petrol", color: "#E42100" },
+  { id: 7, emoji: "", price: "RM200", name: "Saving Plan", color: "#BC2A6E" }
 ];
 
 export default CARDS;
